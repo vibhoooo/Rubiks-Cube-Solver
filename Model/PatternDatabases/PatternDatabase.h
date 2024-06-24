9f204318dc751f50076d11af3c6af8ef3e45ace9
vibhoooo <cse21105@iiitkalyani.ac.in>
2024-06-25 02:13:36 +0530

    models updated

---
+++ Model/PatternDatabases/PatternDatabase.h
@@ -0,0 +1,49 @@
+#ifndef RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+#define RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+
+#include "../../bits/stdcpp.h"
+#include "../../Model/GenericRubiksCube.h"
+#include "../../PatternDatabases/NibbleArray.h"
+
+using namespace std;
+
+class PatternDatabase {
+
+    NibbleArray database;
+    size_t size;
+    size_t numItems;
+
+    PatternDatabase();
+
+public:
+    PatternDatabase(const size_t size);
+//    Testing for init_val
+    PatternDatabase(const size_t size, uint8_t init_val);
+
+    virtual uint32_t getDatabaseIndex(const GenericRubiksCube &cube) const = 0;
+
+    virtual bool setNumMoves(const GenericRubiksCube &cube, const uint8_t numMoves);
+
+    virtual bool setNumMoves(const uint32_t ind, const uint8_t numMoves);
+
+    virtual uint8_t getNumMoves(const GenericRubiksCube &cube) const;
+
+    virtual uint8_t getNumMoves(const uint32_t ind) const;
+
+    virtual size_t getSize() const;
+
+    virtual size_t getNumItems() const;
+
+    virtual bool isFull() const;
+
+    virtual void toFile(const string &filePath) const;
+
+    virtual bool fromFile(const string &filePath);
+
+    virtual vector<uint8_t> inflate() const;
+
+    virtual void reset();
+};
+
+
+#endif //RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
