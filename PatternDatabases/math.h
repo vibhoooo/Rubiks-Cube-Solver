7e3a412b1e61197c55a1edc109c608c638c7b539
vibhoooo <cse21105@iiitkalyani.ac.in>
2024-06-25 02:03:37 +0530

    addded math base class

---
+++ PatternDatabases/math.h
@@ -0,0 +1,11 @@
+#ifndef RUBIKS_CUBE_SOLVER_MATH_H
+#define RUBIKS_CUBE_SOLVER_MATH_H
+
+
+#include <cstdint>
+
+uint32_t factorial(uint32_t n);
+uint32_t pick(uint32_t n, uint32_t k);
+uint32_t choose(uint32_t n, uint32_t k);
+
+#endif //RUBIKS_CUBE_SOLVER_MATH_H
